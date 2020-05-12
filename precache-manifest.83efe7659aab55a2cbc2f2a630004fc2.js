c50321f3f6314cd52fb7f951a801b03ab759540f
Faisal Ahmed <58918568+faisal12101123@users.noreply.github.com>
2020-05-13 00:14:34 +0200

    Add files via upload

---
+++ precache-manifest.83efe7659aab55a2cbc2f2a630004fc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "250468b5cf9414910c81f12c7d47bc85",
+    "url": "/faisal12101123.github.io/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d7da2def62a9f988f205",
+    "url": "/faisal12101123.github.io/monster-rolodex/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/faisal12101123.github.io/monster-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/faisal12101123.github.io/monster-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7da2def62a9f988f205",
+    "url": "/faisal12101123.github.io/monster-rolodex/static/js/main.2b3b74cd.chunk.js"
+  },
+  {
+    "revision": "80a92b3ea44920dfa8ee",
+    "url": "/faisal12101123.github.io/monster-rolodex/static/js/runtime-main.8b747814.js"
+  }
+]);
